b0e5a02f9d2269a82add33b930178acb0a5ac03e
Brevia Studio <breviastudio@gmail.com>
2025-01-06 16:48:53 +0530

    response

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -42,7 +42,7 @@ export const ChatBox = () => {
             {isLoading ? (
               <TypingAnimation text="..." delay={100} infinite styles={"font-bold"} />
             ) : response ? (
-              <TypingAnimation text={response} delay={70} />
+              <TypingAnimation text={response} delay={150} />
             ) : (
               <span>AI response will appear here...</span>
             )}
